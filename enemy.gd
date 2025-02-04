002b7512cabb29972345a312960d7c3a7c6b00d2
sirbread <gverma0824@outlook.com>
2025-02-04 03:57:41 -0700

    added enemies
    wansn't able to get bullets to work, i swear the code is right, ill look at it when it's not 4 am

---
+++ enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+var motion = Vector2()
+
+func _ready():
+	pass
+	
+func _physics_process(delta):
+	var Player = get_parent().get_node("player")
+
+	position += (Player.position - position)/50
+	look_at(Player.position)
+	move_and_collide(motion)
+
+
+
+func _on_area_2d_body_entered(body):
+	if "bullet" in body.name:
+		print("aaaaaaaa")
+		queue_free()
